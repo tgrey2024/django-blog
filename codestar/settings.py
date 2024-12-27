aabcc8614b6a4acb31a9239b9cf3108ce4f38bba
Tripta Grey <tripta.grey@gmail.com>
2024-12-27 16:36:59 +0000

    add post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-tgrey2024-djangoblog-kywwux22jiz.ws.codeinstitute-ide.net', '.herokuapp.com']
 
