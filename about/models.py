5c44de186b3ff76ddafbcccdf02a623ec5f6a34c
Tripta Grey <tripta.grey@gmail.com>
2025-01-01 18:42:10 +0000

    add collaboration request form to aboutapp

--- about/models.py
+++ about/models.py
@@ -1,13 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class About(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    
-    # returns f-string with title from dataset
+
     def __str__(self):
         return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 def about_me(request):
     """
@@ -7,10 +9,25 @@ def about_me(request):
     
     """
 
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            # collaborateRequest = collaborate_form.save(commit=False)
+            # collaborateRequest.read = False
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
     about = About.objects.all().order_by('updated_on').first()
     
+    collaborate_form = CollaborateForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
